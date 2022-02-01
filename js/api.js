b42fd152d8b06ebf0b775416616245c6cecdc0f2
peng <robertjustinianpeng@gmail.com>
2022-01-31 17:19:39 -0700

    initial commit

---
+++ js/api.js
@@ -0,0 +1,2 @@
+const API_KEY = "75bb935b7e7e613ed2bfa73881780b92-us20";
+export default API_KEY;
---
+++ js/index.js
@@ -0,0 +1,4 @@
+import API_KEY from "./api.js";
+
+// Add to build
+// echo -e "const API_KEY = '75bb935b7e7e613ed2bfa73881780b92-us20';\n\nexport default API_KEY;" > test.js
